b9c43b0aea8a5d69bd4ac30ea95175001ab5b0f5
Aaryan Vashishta <aaryan.25b01010409@abes.ac.in>
2026-07-23 11:54:09 +0530

    initialise commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log('Aaryan Vashishta')
+
+
+function sum(a,b) {
+
+}
+const add = (a,b) => {
+    return a,b;
+};
+const add2 = (a,b) => a+b;
+
+console.log(sum(20,10));
+console.log(add(20,10));
+console.log(add2(20,10));
