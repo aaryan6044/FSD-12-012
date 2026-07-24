485c5818691990635fa794fe64fcc370c2374840
Aaryan Vashishta <aaryan.25b01010409@abes.ac.in>
2026-07-24 14:38:16 +0530

    Event Emitter start

---
+++ eventemitter.js
@@ -0,0 +1,14 @@
+const login = (name) => {
+ console.log(`${name} logged in`);
+};
+const start = () => {
+    console.log("System Start");
+};
+
+const working = (name) => {
+ console.log(`${name} Add items to cart`);
+};
+
+const checkout = (name) => {
+ console.log(`${name} logged out`);
+};
